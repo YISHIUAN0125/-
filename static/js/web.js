10cd98f787039bd52fbb07f58f46fa391408995c
YISHIUAN0125 <t8015nyy3kass@gmail.com>
2024-05-01 10:57:39 +0800

    預計寫好五個頁面

---
+++ static/js/web.js
@@ -0,0 +1,20 @@
+let Media = document.getElementsByClassName('Media');
+let Food = document.getElementsByClassName('Food');
+let Money = document.getElementsByClassName('Money');
+let Travel = document.getElementsByClassName('Travel');
+let Fashion = document.getElementsByClassName('Fashion');
+let logo = document.getElementsByClassName('logo');
+
+Media[0].addEventListener('click',web2_);
+Food[0].addEventListener('click',web2_);
+Money[0].addEventListener('click',web2_);
+Travel[0].addEventListener('click',web2_);
+Fashion[0].addEventListener('click',web2_);
+logo[0].addEventListener('click',web1_);
+
+function web2_(){
+    window.location.href = "web_2.html";
+}
+function web1_(){
+    window.location.href = "web_1.html";
+}
---
+++ static/js/web_2.js
@@ -0,0 +1,8 @@
+let logo = document.getElementsByClassName('logo');
+
+
+
+logo[0].addEventListener('click',web1_);
+function web1_(){
+    window.location.href = "web_1.html";
+}
